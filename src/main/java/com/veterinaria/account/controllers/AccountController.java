91f5ae785bacaa350fd533060c7e6565a339085f
JuanMira <juan@remotepcsolutions.com>
2021-11-22 08:09:02 -0500

    Agregando modificicaciones funcionalidad de crear, con la fecha actual lista de cuentas en proceso de crear la de eliminaar pendiente editar

--- src/main/java/com/veterinaria/account/controllers/AccountController.java
+++ src/main/java/com/veterinaria/account/controllers/AccountController.java
@@ -1,5 +1,10 @@
 package com.veterinaria.account.controllers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+
 import javax.security.auth.login.AccountNotFoundException;
 
 import com.veterinaria.account.models.Account;
@@ -17,12 +22,23 @@ public class AccountController {
 
     @GetMapping("/account/{username}")
     Account getAccount(@PathVariable String username) throws AccountNotFoundException {
-        return accountRepository.findById(username).orElseThrow(
-                () -> new AccountNotFoundException("No se encontro una cuenta con el username: " + username));
+        return accountRepository.findById(username)
+                .orElseThrow(() -> new AccountNotFoundException("Usuario no encontrado: " + username));
     }
 
     @PostMapping("/account")
     Account newAccount(@RequestBody Account account) {
+        account.setLastChange(new Date());
         return accountRepository.save(account);
+    }
+
+    @GetMapping("/account")
+    List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    @DeleteMapping("/account")
+    void deleteAccount(@PathVariable String idUsuario) {
+
     }
 }
