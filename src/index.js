d06f443d4707afec4dce9196135edfe294e0997e
JuanMira <juan@remotepcsolutions.com>
2021-11-23 08:55:14 -0500

    Estructura de archivos base

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import constants from "./constants";
-import pkg from "./package.json";
+import pkg from "../package.json";
 const app = express();
 
 app.get("/", (req, res) => {
