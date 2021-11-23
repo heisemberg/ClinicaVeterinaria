bc266df92d8dd0ad543e87b2c7f1c9becfb81b4c
JuanMira <juan@remotepcsolutions.com>
2021-11-23 07:16:07 -0500

    Empaquetar informacion

--- src/main/java/com/veterinaria/account/controllers/AccountController.java
+++ src/main/java/com/veterinaria/account/controllers/AccountController.java
@@ -1,7 +1,10 @@
 package com.veterinaria.account.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.security.auth.login.AccountNotFoundException;
 
@@ -36,7 +39,22 @@ public class AccountController {
     }
 
     @DeleteMapping("/account")
-    void deleteAccount(@PathVariable String idUsuario) {
+    Map<String, String> deleteAccount(@PathVariable String idUsuario) {
+        HashMap<String, String> res = new HashMap<String, String>();
+        Optional<Account> exist = accountRepository.findById(idUsuario);
+        if (exist.isPresent()) {
+            res.put("message", "borrado satisfactoriamente");
+            accountRepository.deleteById(idUsuario);
+            return res;
+        }
+        res.put("message", "usuario no existe");
+        return res;
+    }
 
+    @PutMapping("/account/{id}")
+    Map<String, String> updateAccount(@PathVariable String idUsuario) {
+        HashMap<String, String> res = new HashMap<String, String>();
+        res.put("message", "actualizado satisfactoriamente");
+        return res;
     }
 }
